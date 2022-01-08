5b69611dde8384468392db788e6a4680cdd47141
RavenKMusic <85319728+ravenkmusic@users.noreply.github.com>
2022-01-08 16:42:02 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,20 @@
+#Write your code below this line 👇
+#Hint: Remember to import the random module first. 🎲
+
+import random
+
+coin = random.randint(0, 1)
+
+if coin == 0:
+  print("Heads!")
+
+if coin == 1:
+  print("Tales!")
+
+
+
+
+
+
+
+
